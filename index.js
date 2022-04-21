8dd654aaebc63885c52c80f40b941027f99b095b
Ulysses Pinheiro <ulyssespinheiro00@gmail.com>
2022-04-21 10:47:01 -0300

    Game traslated to english

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var difficulty = 4;
-var audioCardFlip = new Audio('Sounds/f4ngy-card-flip.wav');
-var winSound = new Audio('Sounds/win-sound.wav');
+var audioCardFlip = new Audio("Sounds/f4ngy-card-flip.wav");
+var winSound = new Audio("Sounds/win-sound.wav");
 var flipSpeed = 400; // changes the time it takes to flip the cards
 var allowclick = true; // checks if anything is runnning befor the user is allowed to click the button
 
 //===============================Display Cards=================================//
 function displayCards(difficulty) {
   for (let i = 1; i < gamePattern.length + 1; i++) {
-    $("#reference").after("<div class='card-div card-div-size1'><input class='card " + i + "' onclick='buttoclicked(" + i + ")' type='image' src='Imgs/card.png'></div>");
+    $("#reference").after(
+      "<div class='card-div card-div-size1'><input class='card " +
+        i +
+        "' onclick='buttoclicked(" +
+        i +
+        ")' type='image' src='Imgs/card.png'></div>"
+    );
   }
 }
 
@@ -23,8 +29,6 @@ function resizeGameDiv(difficulty) {
 
     case 18:
       $("#Game-Cards").width(1000);
-      // $(".card-div").removeClass("card-div-size1 card-div-size2 card-div-size3 card-div-size4");
-      // $(".card-div").toggleClass("card-div-size2");
       break;
 
     case 35:
@@ -32,41 +36,48 @@ function resizeGameDiv(difficulty) {
       break;
 
     default:
-      // code block
   }
 }
 
 function resizeGameCards(difficulty) {
   switch (difficulty) {
     case 4:
-      $(".card-div").removeClass("card-div-size1 card-div-size2 card-div-size3 card-div-size4");
+      $(".card-div").removeClass(
+        "card-div-size1 card-div-size2 card-div-size3 card-div-size4"
+      );
       $(".card-div").toggleClass("card-div-size1");
       break;
 
     case 9:
-      $(".card-div").removeClass("card-div-size1 card-div-size2 card-div-size3 card-div-size4");
+      $(".card-div").removeClass(
+        "card-div-size1 card-div-size2 card-div-size3 card-div-size4"
+      );
       $(".card-div").toggleClass("card-div-size2");
       break;
 
     case 18:
-      $(".card-div").removeClass("card-div-size1 card-div-size2 card-div-size3 card-div-size4");
+      $(".card-div").removeClass(
+        "card-div-size1 card-div-size2 card-div-size3 card-div-size4"
+      );
       $(".card-div").toggleClass("card-div-size3");
       break;
 
     case 35:
-      $(".card-div").removeClass("card-div-size1 card-div-size2 card-div-size3 card-div-size4");
+      $(".card-div").removeClass(
+        "card-div-size1 card-div-size2 card-div-size3 card-div-size4"
+      );
       $(".card-div").toggleClass("card-div-size4");
       break;
 
     default:
-      // code block
+    // code block
   }
 }
 
 //===============================Game Win======================================//
 function gameWin() {
-  setTimeout(function() {
-    if (($(".locked").length) === (difficulty * 2)) {
+  setTimeout(function () {
+    if ($(".locked").length === difficulty * 2) {
       winSound.play();
     }
   }, 3000);
@@ -77,22 +88,24 @@ function restartGame(difficulty) {
   for (let i = 1; i <= gamePattern.length; i++) {
     flipCardCover(i);
   }
-  setTimeout(function() {
+  setTimeout(function () {
     resizeGameDiv(difficulty);
     firstCard = true;
     lastCard = "";
     iniGamePattern = [];
     gamePattern = [];
     $(".card-div").remove();
-    generateGameCards(difficulty); //Regenerates the game cards
-    if ($(".card").hasClass("flip")) { // removes flip states from cards
+    generateGameCards(difficulty);
+    if ($(".card").hasClass("flip")) {
+      // removes flip states from cards
       $(".card").removeClass("flip");
     }
     for (let i = 1; i <= difficulty * 2; i++) {
       flipCardCover(i);
     }
     resizeGameCards(difficulty);
-    if ($(".card").hasClass("locked")) { // removes locks from cards
+    if ($(".card").hasClass("locked")) {
+      // removes locks from cards
       $(".card").toggleClass("locked");
     }
   }, flipSpeed);
@@ -101,29 +114,33 @@ function restartGame(difficulty) {
 function changeDifficulty(newDif) {
   difficulty = newDif;
   restartGame(difficulty);
-  $(".difficulty-selector-button").removeClass("btn-light btn-outline-success btn-outline-warning btn-outline-danger btn-outline-dark");// removes all previous classes
-switch (difficulty){             // applies the current difficulty to the selector
-  case 4:
-    $(".difficulty-selector-button").toggleClass("btn-outline-success");
-    $(".difficulty-selector-button").text("Fácil");
-    break;
+  $(".difficulty-selector-button").removeClass(
+    "btn-light btn-outline-success btn-outline-warning btn-outline-danger btn-outline-dark"
+  ); // removes all previous classes
+  switch (
+    difficulty // applies the current difficulty to the selector
+  ) {
+    case 4:
+      $(".difficulty-selector-button").toggleClass("btn-outline-success");
+      $(".difficulty-selector-button").text("Easy");
+      break;
 
-  case 9:
-    $(".difficulty-selector-button").toggleClass("btn-outline-warning");
-    $(".difficulty-selector-button").text("Médio");
-    break;
+    case 9:
+      $(".difficulty-selector-button").toggleClass("btn-outline-warning");
+      $(".difficulty-selector-button").text("Medium");
+      break;
 
-  case 18:
-    $(".difficulty-selector-button").toggleClass("btn-outline-danger");
-    $(".difficulty-selector-button").text("Difícil");
-    break;
+    case 18:
+      $(".difficulty-selector-button").toggleClass("btn-outline-danger");
+      $(".difficulty-selector-button").text("Hard");
+      break;
 
-  case 35:
-    $(".difficulty-selector-button").toggleClass("btn-outline-dark");
-    $(".difficulty-selector-button").text("Extremo");
-    break;
-}
-    $(".difficulties").toggleClass("hidden"); // hides the expanded menu
+    case 35:
+      $(".difficulty-selector-button").toggleClass("btn-outline-dark");
+      $(".difficulty-selector-button").text("Extreme");
+      break;
+  }
+  $(".difficulties").toggleClass("hidden"); // hides the expanded menu
 }
 
 function expanddifficultyMenu() {
@@ -134,31 +151,36 @@ function expanddifficultyMenu() {
 var firstCard = true;
 var lastCard = "";
 
-
 //detecting button presses
-function buttoclicked(button) { //usuário clica na carta
+function buttoclicked(button) {
+  //usuário clica na carta
   if (allowclick === true) {
-    if ($("." + button + ".card").hasClass("locked") === false) { //A Carta está travada?
+    if ($("." + button + ".card").hasClass("locked") === false) {
+      //A Carta está travada?
       //alert("Card is not Locked");
-      if (firstCard === true) { //É a primeira carta?
+      if (firstCard === true) {
+        //É a primeira carta?
         //alert("First Card");
         flipCardInsect(button);
         $("." + button + ".card").toggleClass("locked");
         lastCard = button; // saves the last card
         firstCard = false;
-      } else { //Não é a primeira carta
+      } else {
+        //Não é a primeira carta
         //alert("Not the first Card");
-        if (gamePattern[button - 1] === gamePattern[lastCard - 1]) { // é igual a carta anterior
+        if (gamePattern[button - 1] === gamePattern[lastCard - 1]) {
+          // é igual a carta anterior
           //alert("same as last card:" + gamePattern[button - 1] + " and " + gamePattern[lastCard - 1]);
           flipCardInsect(button);
           $("." + button + ".card").toggleClass("locked"); // trava a Carta
           firstCard = true; // libera para selecionar o próximo par
           gameWin();
-        } else if (firstCard === false) { //Não é igual a ultima Carta
+        } else if (firstCard === false) {
+          //Não é igual a ultima Carta
           //alert("Not the same as the last card: " + gamePattern[button - 1] + " and " + gamePattern[lastCard - 1]);
           flipCardInsect(button);
           allowclick = false;
-          setTimeout(function() {
+          setTimeout(function () {
             flipCardCover(button);
             flipCardCover(lastCard);
             allowclick = true;
@@ -176,10 +198,13 @@ function buttoclicked(button) { //usuário clica na carta
 function flipCardInsect(button) {
   $("." + button + ".card").toggleClass("flip");
   audioCardFlip.play();
-  setTimeout(function() {
-    $("." + button + ".card").attr("src", "Imgs/Insects/" + gamePattern[button - 1] + ".png");
+  setTimeout(function () {
+    $("." + button + ".card").attr(
+      "src",
+      "Imgs/Insects/" + gamePattern[button - 1] + ".png"
+    );
   }, flipSpeed);
-  setTimeout(function() {
+  setTimeout(function () {
     $("." + button + ".card").toggleClass("flip");
   }, flipSpeed);
   openCards = [button];
@@ -189,22 +214,57 @@ function flipCardInsect(button) {
 function flipCardCover(button) {
   $("." + button + ".card").toggleClass("flip");
   audioCardFlip.play();
-  setTimeout(function() {
+  setTimeout(function () {
     $("." + button + ".card").attr("src", "Imgs/card.png");
   }, flipSpeed);
-  setTimeout(function() {
+  setTimeout(function () {
     $("." + button + ".card").toggleClass("flip");
   }, flipSpeed);
   openCards = [];
 }
 
-
 //=========================GAME PATTERN GENERATOR==============================//
 // There are 39 items in the array
-var avaiableInsects = ['ant', 'bed-bug', 'beetle', 'butterfly blue', 'butterfly', 'caterpillar', 'centipede', 'cicada', 'cockroach', 'cricket', 'dragonfly',
-  'firebug', 'flea', 'fly', 'grasshopper', 'green-fly', 'grub', 'leaf-insect', 'madagascar-hissing-cockroach', 'maggot', 'mie', 'mosquito', 'moth', 'praying-mantis',
-  'rain-beetle', 'scorpion', 'snake-fly', 'soldier-termite', 'spider', 'stink-bug', 'stonefly', 'termite', 'tick', 'tomcat', 'walking-stick', 'wasp', 'wolf-spider',
-  'woodlouse', 'worm'
+var avaiableInsects = [
+  "ant",
+  "bed-bug",
+  "beetle",
+  "butterfly blue",
+  "butterfly",
+  "caterpillar",
+  "centipede",
+  "cicada",
+  "cockroach",
+  "cricket",
+  "dragonfly",
+  "firebug",
+  "flea",
+  "fly",
+  "grasshopper",
+  "green-fly",
+  "grub",
+  "leaf-insect",
+  "madagascar-hissing-cockroach",
+  "maggot",
+  "mie",
+  "mosquito",
+  "moth",
+  "praying-mantis",
+  "rain-beetle",
+  "scorpion",
+  "snake-fly",
+  "soldier-termite",
+  "spider",
+  "stink-bug",
+  "stonefly",
+  "termite",
+  "tick",
+  "tomcat",
+  "walking-stick",
+  "wasp",
+  "wolf-spider",
+  "woodlouse",
+  "worm",
 ];
 var iniGamePattern = [];
 var gamePattern = [];
@@ -216,7 +276,9 @@ var gamePattern = [];
 generateGameCards(difficulty);
 
 function createCard() {
-  return avaiableInsects[Math.floor((avaiableInsects.length - 1) * Math.random())];
+  return avaiableInsects[
+    Math.floor((avaiableInsects.length - 1) * Math.random())
+  ];
 }
 
 //Generates game cards, and run createCard X amounts of time, based on difficulty
@@ -226,7 +288,8 @@ function generateGameCards(difficulty) {
 
   var newCard = createCard();
   while (iniGamePattern.length < difficulty) {
-    if (iniGamePattern.includes(newCard) === true) { //checks if he new generated card already exists in the array
+    if (iniGamePattern.includes(newCard) === true) {
+      //checks if he new generated card already exists in the array
       newCard = createCard();
     } else {
       iniGamePattern.push(newCard);
@@ -250,25 +313,16 @@ function shuffle(array) {
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]
+      array[randomIndex],
+      array[currentIndex],
     ];
   }
 
   return array;
 }
-
-
-
-
-
-
-
-
-//end
